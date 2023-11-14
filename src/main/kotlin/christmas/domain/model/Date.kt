b0fc7a5ac439ac2ce8b9397b51f9e2d28b1ad5a3
murjune <sis9221302@gmail.com>
2023-11-14 15:27:12 +0900

    fix(Date) : field -> 생성자로 이동, Parameter 네이밍 변경

--- src/main/kotlin/christmas/domain/model/Date.kt
+++ src/main/kotlin/christmas/domain/model/Date.kt
@@ -1,27 +1,26 @@
 package christmas.domain.model
 
-data class Date(private val dayOfMonth: Int) {
+data class Date(private val dayOfMonth: Int, private val specialDays: List<Int> = listOf(3, 10, 17, 24, 25, 31)) {
     init {
         require(dayOfMonth in DAY_RANGE)
     }
 
-    private val specialDays = listOf(3, 10, 17, 24, 25, 31)
-
-    fun isWeekday() = (dayOfMonth % MOD_UNIT) in WEEKDAY_MOD_RANGE
+    fun isWeekday() = ((dayOfMonth + DAY_WEIGHT_UNIT) % MOD_UNIT) in WEEKDAY_RANGE
 
     fun isSpecial() = dayOfMonth in specialDays
 
     fun isBefore(dayOfMonth: Int) = this.dayOfMonth < dayOfMonth
 
-    fun isAfter(day: Int) = this.dayOfMonth > dayOfMonth
+    fun isAfter(dayOfMonth: Int) = this.dayOfMonth > dayOfMonth
 
     companion object {
+        private const val DAY_WEIGHT_UNIT = 6
         private const val START_DAY = 1
         private const val END_DAY = 31
         private val DAY_RANGE = START_DAY..END_DAY
-        private const val WEEKDAY_START_MOD = 3
-        private const val WEEKDAY_END_MOD = 7
-        private val WEEKDAY_MOD_RANGE = WEEKDAY_START_MOD..WEEKDAY_END_MOD
+        private const val START_WEEKDAY = 2
+        private const val END_WEEKDAY = 6
+        private val WEEKDAY_RANGE = START_WEEKDAY..END_WEEKDAY
         private const val MOD_UNIT = 7
     }
 }
