4e4e4c67f285e500819e4ffd98339e66da59f1a2
murjune <sis9221302@gmail.com>
2023-11-15 01:42:44 +0900

    fix(DateInputValidator) : Date Input 값이 Blank인지 check

--- src/main/kotlin/christmas/controller/validator/DateInputValidator.kt
+++ src/main/kotlin/christmas/controller/validator/DateInputValidator.kt
@@ -5,12 +5,17 @@ import christmas.domain.type.ErrorType
 class DateInputValidator : InputValidator {
 
     override fun validate(input: String) {
+        validateIsBlank(input)
         validateIsNumeric(input)
     }
-
+    private fun validateIsBlank(date: String) {
+        require(date.isNotBlank()) {
+            ErrorType.DATE.toString()
+        }
+    }
     private fun validateIsNumeric(date: String) {
         require(date.all { it.isDigit() }) {
-            ErrorType.DATE
+            ErrorType.DATE.toString()
         }
     }
 }
