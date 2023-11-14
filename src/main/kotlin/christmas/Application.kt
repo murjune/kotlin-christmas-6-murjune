9821772d5851f52a1850db56c5e73c68c7b9221b
murjune <sis9221302@gmail.com>
2023-11-15 01:46:34 +0900

    feat(Controller): 주문 총 가격, 증정, 혜택, 가격, 배지 View  구현, 어플리케이션 구현

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,7 @@
 package christmas
 
+import christmas.controller.ChristmasPromotionController
+
 fun main() {
-    TODO("프로그램 구현")
+    ChristmasPromotionController().start()
 }
--- src/main/kotlin/christmas/controller/ChristmasPromotionController.kt
+++ src/main/kotlin/christmas/controller/ChristmasPromotionController.kt
@@ -3,8 +3,14 @@ package christmas.controller
 import camp.nextstep.edu.missionutils.Console
 import christmas.controller.error.ErrorHandler
 import christmas.controller.error.InputErrorHandler
+import christmas.controller.format.PriceFormatter
 import christmas.controller.validator.DateInputValidator
+import christmas.domain.model.Badge
 import christmas.domain.model.Date
+import christmas.domain.model.discount.ChristmasDiscounter
+import christmas.domain.model.discount.DailyDiscounter
+import christmas.domain.model.discount.Discount
+import christmas.domain.model.discount.SpecialDiscounter
 import christmas.domain.model.menu.Menu
 import christmas.domain.model.menu.MenuBoard
 import christmas.domain.model.order.TotalOrder
@@ -23,11 +29,19 @@ class ChristmasPromotionController {
     private val orderTransformer = TotalOrderTransformer(menuBoard)
     private lateinit var totalOrder: TotalOrder
     private lateinit var orderMenus: List<Menu>
+    private var totalPrice = 0
+    private var totalProfits = 0
+    private val priceFormatter = PriceFormatter()
 
     fun start() {
         showDateView()
         showOrderView()
         showOrderMenusView()
+        showOrderPriceView()
+        showGiftView()
+        showProfitView()
+        showPaymentView()
+        showEventBadgeView()
     }
 
     private fun showDateView() {
@@ -53,7 +67,7 @@ class ChristmasPromotionController {
         errorHandler.handle(
             action = {
                 outPutView.writeOrderGuide()
-                val input = Console.readLine().split(COMMA)
+                val input = inputView.readOrder()
                 totalOrder = orderTransformer.transform(input).also {
                     orderMenus = it.toMenuList()
                 }
@@ -68,11 +82,75 @@ class ChristmasPromotionController {
         outPutView.writeOrderMenus(totalOrder)
     }
 
-    private companion object {
-        const val COMMA = ","
+    private fun showOrderPriceView() {
+        totalPrice = totalOrder.calculatePrice()
+        outPutView.writeOrderPrice(totalPrice.toPriceFormat())
     }
-}
 
-fun main() {
-    ChristmasPromotionController().start()
+    private fun showGiftView() {
+        val gift = totalOrder.provideGift()?.let {
+            CHAMPAGNE
+        } ?: NO_EVENT
+        outPutView.writeGift(gift)
+    }
+
+    private fun showProfitView() {
+        val discounts = calculateDiscounts()
+        val gift = provideGift()
+        showProfitHistory(discounts, gift)
+        showSumOfProfitView(discounts, gift)
+    }
+
+    private fun showProfitHistory(discounts: List<Discount>, gift: String?) {
+        if (totalOrder.isEventExecutable().not()) return outPutView.writeNoProfit()
+        val discountProfits = discounts.map { it.toString() }
+        val giftProfits = gift?.let { listOf(GIFT_PROFIT_MESSAGE) } ?: listOf()
+        outPutView.writeProfitHistory(discountProfits + giftProfits)
+    }
+
+    private fun showSumOfProfitView(discounts: List<Discount>, gift: String?) {
+        if (totalOrder.isEventExecutable().not()) return outPutView.writeSumOfProfit(0.toPriceFormat())
+        val discountProfits = Discount.sumOfProfit(discounts)
+        val giftProfit = gift?.let { GIFT_PROFIT } ?: NO_PROFIT
+        totalProfits = discountProfits + giftProfit
+        outPutView.writeSumOfProfit(totalProfits.toProfitFormat())
+    }
+
+    private fun showPaymentView() {
+        val payment = totalPrice - totalProfits
+        outPutView.writePaymentPreview(payment.toPriceFormat())
+    }
+
+    private fun showEventBadgeView() {
+        val badge = Badge.find(totalProfits)
+        outPutView.writeEventBadge(badge)
+    }
+
+    private fun provideGift(): String? = totalOrder.provideGift()?.let {
+        GIFT_PROFIT_MESSAGE
+    }
+
+    private fun calculateDiscounts(): List<Discount> {
+        return mutableListOf<Discount>().apply {
+            val christmasDiscount = ChristmasDiscounter(today, dayOfMonth).discount()
+            val dailyDiscount = DailyDiscounter(today, orderMenus).discount()
+            val specialDiscount = SpecialDiscounter(today).discount()
+            if (christmasDiscount.canApply()) add(christmasDiscount)
+            if (dailyDiscount.canApply()) add(dailyDiscount)
+            if (specialDiscount.canApply()) add(specialDiscount)
+        }
+    }
+
+    private fun Int.toPriceFormat() = priceFormatter.format(this) + WON
+    private fun Int.toProfitFormat() = MINUS + priceFormatter.format(this) + WON
+
+    private companion object {
+        const val MINUS = "-"
+        const val WON = "원"
+        const val CHAMPAGNE = "샴페인 1개"
+        const val NO_EVENT = "없음"
+        const val GIFT_PROFIT_MESSAGE = "증정 이벤트: -25,000원"
+        const val GIFT_PROFIT = 25000
+        const val NO_PROFIT = 0
+    }
 }
