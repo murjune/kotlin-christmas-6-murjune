82a2fd255c0912a69f8975af8b3f293c7fdef685
murjune <sis9221302@gmail.com>
2023-11-14 20:02:26 +0900

    ui(InputView, outputView) : 입출력 뷰

---
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -0,0 +1,13 @@
+package christmas.view
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+    fun readDate(input: String = Console.readLine()): String {
+        return input
+    }
+
+    fun readOrder(input: String = Console.readLine()): String {
+        return input
+    }
+}
