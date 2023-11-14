96f6d68fae841cba434ab131b5ca8e2789653272
murjune <sis9221302@gmail.com>
2023-11-15 01:43:22 +0900

    style(InputView) : expression으로 수정

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -3,11 +3,11 @@ package christmas.view
 import camp.nextstep.edu.missionutils.Console
 
 class InputView {
-    fun readDate(input: String = Console.readLine()): String {
-        return input
-    }
+    fun readDate(input: String = Console.readLine()) = input
 
-    fun readOrder(input: String = Console.readLine()): String {
-        return input
+    fun readOrder(input: String = Console.readLine()) = input.split(COMMA)
+
+    companion object {
+        const val COMMA = ","
     }
 }
